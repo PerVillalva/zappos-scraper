d269c6e76a5981aca9715bfc9e8670c7d8d56bf9
pvillalva <pervillalva@gmail.com>
2022-11-13 18:29:00 +0100

    Improved code modularity

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 // For more information, see https://crawlee.dev/
 import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
-import { LABELS } from "./consts.js";
 
 const crawler = new CheerioCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
 });
 
+const searchQuery = "men oxfords".toLowerCase();
+
 await crawler.addRequests([
-    "https://www.zappos.com/drinkware/.zso?t=drinkware",
+    `https://www.zappos.com/${searchQuery
+        .trim()
+        .replace(" ", "-")}/.zso?t=${encodeURIComponent(
+        searchQuery.trim()
+    )}&p=0`,
 ]);
 
 await crawler.run();
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,25 @@
-import { Dataset, createCheerioRouter } from "crawlee";
+import { Dataset, createCheerioRouter, CrawlerExtension } from "crawlee";
 import { LABELS } from "./consts.js";
 
 export const router = createCheerioRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log, $, request }) => {
+router.addDefaultHandler(async ({ enqueueLinks, log, $, request, crawler }) => {
     const currentPage = $('span[aria-current="true"]').text();
+    const lastPage = Number($("span.Wk-z a:last-child").text());
     if (currentPage === "1") {
         const totalProducts = $("span.jJ-z").text();
         log.info(totalProducts);
+        for (let page = 1; page < lastPage; page++) {
+            await crawler.requestQueue?.addRequests([
+                {
+                    url: request.url.replace(/p\=\d+/, `p=${page}`),
+                    label: LABELS.LIST,
+                },
+            ]);
+        }
     }
-    log.info(`Enqueueing pagination: ${request.url}`);
-    await enqueueLinks({
-        globs: ["https://www.zappos.com/drinkware/.zso?t=drinkware&p=*"],
-        label: LABELS.LIST,
-    });
 
-    log.info(`Enqueueing product details: ${request.url}`);
+    log.info(`Enqueueing product details: ${request.loadedUrl}`);
     await enqueueLinks({
         selector: 'a[itemprop="url"]',
         label: LABELS.PRODUCT,
@@ -24,12 +28,47 @@ router.addDefaultHandler(async ({ enqueueLinks, log, $, request }) => {
 
 router.addHandler(LABELS.PRODUCT, async ({ request, $, log }) => {
     log.info(`Extracting data: ${request.url}`);
-    await Dataset.pushData({
+
+    // ✅ Add specific properties to object
+    interface Product {
+        url: string;
+        brand: string;
+        name: string;
+        SKU: string;
+        inStock: boolean;
+        onSale: boolean;
+        price: string;
+        originalPrice?: string; // 👈️ mark as optional so we can include it only when the product is on sale
+    }
+
+    const priceElement = $('span[itemprop="price"]');
+
+    const results: Product = {
+        // @ts-ignore
         url: request.loadedUrl,
-        brand: $('a[itemprop="url"] span').text(),
-        name: $('meta[itemprop="name"]').attr("content"),
-        SKU: $('span[itemprop="sku"]').text(),
-        currentPrice: `$${$('span[itemprop="price"]').attr("content")}`,
-        originalPrie: $(".SA-z").text(),
-    });
+        brand: $('span[itemprop="brand"]').text().trim(),
+        // @ts-ignore
+        name: "",
+        SKU: $('*[itemprop~="sku"]').text().trim(),
+        inStock: true,
+        onSale: false,
+        price: `${priceElement.text()}`,
+    };
+
+    if (priceElement.hasClass("JA-z")) {
+        results.onSale = true;
+        results.originalPrice = $(".SA-z").text();
+    }
+
+    if (request.url.includes("oosRedirected=true")) {
+        results.inStock = false;
+    }
+
+    if (request.url.includes("the-style-room")) {
+        results.name = $('*[itemprop~="name"]').text().trim();
+    } else {
+        // @ts-ignore
+        results.name = $("span.EW-z").text().trim();
+    }
+    await Dataset.pushData(results);
 });
